aae6b4a0bc2a5a3d385f883c2631672d574ab857
Ollie Baker <oijbaker@outlook.com>
2022-11-29 10:56:20 +0000

    Added portfolio class

--- portfolio.py
+++ portfolio.py
@@ -9,16 +9,22 @@ class Portfolio:
        
         
     def buy(self, stock_index, number):
+        """ Buy a stock
+
+        Args:
+            stock_index (int): This needs to be 
+            number (_type_): _description_
+        """
         volume_field = "vol"+str(stock_index)
         close_field = "close"+str(stock_index)
         
         volume = self.df[volume_field][self.day]
         close = self.df[close_field][self.day]
         
-        if number < 0.4*volume and number*close < self.cash:
+        if number < 0.4*volume and number*close < self.cash*0.99:
             self.stocks[stock_index-1][1] += number
             
-        self.cash -= number*close
+        self.cash -= number*close*0.99
      
             
     def sell(self, stock_index, number):
@@ -28,10 +34,10 @@ class Portfolio:
         volume = self.df[volume_field][self.day]
         close = self.df[close_field][self.day]
         
-        if number <= self.stocks[stock_index-1][1] and number < 0.4*volume:
+        if number <= self.stocks[stock_index-1][1]*0.99 and number < 0.4*volume:
             self.stocks[stock_index-1][1] -= number
             
-        self.cash += number*close
+        self.cash += number*close*0.99
         
         
     def evaluate(self):
--- test_portfolio.py
+++ test_portfolio.py
@@ -5,5 +5,5 @@ df = pd.read_csv("df_moving_averages.csv").drop("Unnamed: 0", axis=1)
 p = portfolio.Portfolio(df)
 
 p.buy(4, 10)
-p.buy(1, 2)
+p.sell(4, 9.9)
 print(p.evaluate())
