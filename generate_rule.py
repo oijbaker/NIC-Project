177e6609d1dd9da8383120011a5e662a132e5610
joshua-yeung <79918553+joshua-yeung@users.noreply.github.com>
2022-12-08 21:52:33 +0000

    Add files via upload

--- generate_rule.py
+++ generate_rule.py
@@ -3,78 +3,54 @@ import numpy as np
 import pandas as pd
 from collections import deque
 import random
-import rules
 import functions
 
 """
 structure of a rule 
 
 		or 
-	>      	  	<
 
-   avg	    50	     max   30 
+	>      	  <
+
+avg	 max   max   cp 
+ |    |     |     
+ 50   25    10   
 
 The above rule state if moving average over 50 days is greater than closing price buy or sell otherwise
-or if max price over 30 days is less than closing price buy or sell otherwise. 
+or if max price over 30 days is greater than closing price buy or sell otherwise. 
 
 """
-
-def moving_average(stock, days, day):
-    return df["avg"+str(days)+"_"+str(stock+1)][day]
-
-def max_(stock, days, day):
-    return df["max"+str(days)+"_"+str(stock+1)][day]
-
-def min_(stock, days, day):
-    return df["min"+str(days)+"_"+str(stock+1)][day]
-
 df = pd.read_csv("complete_data.csv").drop("Unnamed: 0",1)
 
-add = functions.Function("+", 2, lambda x: x[0]+x[1])
-subtract = functions.Function("-", 2, lambda x: x[0]-x[1])
-multiply = functions.Function("*", 2, lambda x: x[0]*x[1])
-greater = functions.Function(">", 2, lambda x: x[0]>x[1])
 or_ = functions.Function("or", 2, lambda x: (x[0] or x[1]))
 and_ = functions.Function("and", 2, lambda x: x[0] and x[1])
-avg = functions.Function("avg", 0, lambda: rules.moving_average(random.randint(1,50)))
-max_ = functions.Function("max", 0, lambda: rules.max_price(random.randint(1,50)))
-min_ = functions.Function("min", 0, lambda: rules.min_price(random.randint(1,50)))
-functions = [add, subtract, multiply, or_, and_, avg, max_, min_]
+functions = [ or_, and_]
 func_dict = {}
 for function in functions:
 	func_dict[function.name] = function
- 
-def evaluate(f1, f2, operator, df):
-	bool_arr = []
-	for i in range(1, 11):
-		if operator == '>':
-			bool_arr.append(np.where(df[f1 + str('_') + str(i)] > df[f2 + str('_') + str(i)], True, False))
-		else:
-			bool_arr.append(np.where(df[f1 + str('_') + str(i)] < df[f2 + str('_') + str(i)], True, False))
-	return bool_arr
+
 
 
 def generate_random_rule():
-	function_array = ['avg5', 'avg10', 'avg15', 'avg25', 'avg50', 'max5', 'max10', 'max15', 'max25', 'max50', 'min5', 'min10', 'min15', 'min25', 'min50']
+	# function_array contains funtion that will be used to generate a rule 
+	# avg5 means the moving average over 5 days 
+	function_array = ['avg5', 'max5', 'min5','avg10', 'max10', 'min10','avg15', 'max15', 'min15', 'avg25', 'max25', 'min25', 'avg50', 'max50', 'min50']
 	boolean_operators = ['and', 'or']
-	relational_operators = ['>', '<']
+	relational_operators = ['>', '<'] # for comparisons
 
 	function_idx = []
 	boolean_idx = random.randint(0, len(boolean_operators) -1)
 	relational_idx = []
-	random_param = []
 
 	for i in range(2):
 		f_idx = random.randint(0, len(function_array)-1)
 		r_idx = random.randint(0, len(relational_operators) -1)
-		param = random.randint(0, 50)
 		function_idx.append(f_idx)
 		relational_idx.append(r_idx)
-		
 
-
+	# root node should be a boolean operator
 	root = Node(boolean_operators[boolean_idx])
-
+	# followed by relational operator
 	root.left = Node(relational_operators[relational_idx[0]])
 	root.right = Node(relational_operators[relational_idx[1]])
 
@@ -82,13 +58,15 @@ def generate_random_rule():
 	l1 = Node(random.choice(function_array))
 	r0 = Node(random.choice(function_array))
 	r1 = Node(random.choice(function_array))
- 
+ 	
+ 	# leaf node should be functions 
 	root.left.add(l0, 'l')
 	root.left.add(l1, 'r')
 	root.right.add(r0, 'l')
 	root.right.add(r1, 'r')
 
 	return root 
+	
 
 def inorderIterative(root):
 	stack = deque()
@@ -114,13 +92,23 @@ def get_subtree(root):
 		
 	return sub_tree
 
+def evaluate(f1, f2, operator, df):
+	bool_arr = []
+	for i in range(1, 11):
+		if operator == '>':
+			bool_arr.append(np.where(df[f1 + str('_') + str(i)] > df[f2 + str('_') + str(i)], True, False))
+		else:
+			bool_arr.append(np.where(df[f1 + str('_') + str(i)] < df[f2 + str('_') + str(i)], True, False))
+	return bool_arr
+
 
 def evaluate_tree(tree):
+	# evaluate left sub-tree 
     f1 = tree.left.left.data
     f2 = tree.left.right.data
     operator = tree.left.data
     left = evaluate(f1, f2, operator, df)
-    
+    # evaluate right sub-tree 
     f1 = tree.right.left.data
     f2 = tree.right.right.data
     operator = tree.right.data
@@ -128,13 +116,13 @@ def evaluate_tree(tree):
     
     boolean = func_dict[tree.data]
     return [[boolean.execute([left[i][j], right[i][j]]) for j in range(len(left[i]))] for i in range(len(left))]
-    
-    
+
 
 def main():
 	rule = generate_random_rule()
 	arr = inorderIterative(rule)
 	return rule
+
 
 if __name__ == '__main__':
 	main()
