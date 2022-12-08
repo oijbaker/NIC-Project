7f56560bf5531f8cf1e8e57862dc312af806e42e
joshua-yeung <79918553+joshua-yeung@users.noreply.github.com>
2022-12-08 21:54:18 +0000

    Add files via upload

--- rules.py
+++ rules.py
@@ -6,51 +6,33 @@ def read_file(file):
 	df = pd.read_csv(file, header = None, names = cols )
 	return df
 
-
 def moving_average(days, df):
 	my_df = df.copy()
 	for i in range(1, 11):
 		my_df['ma_close' + str(i)] = my_df['close' + str(i)].shift(1).rolling(window=days).mean()
-	my_df.dropna(inplace = True)
+		my_df['ma_close' + str(i)] = my_df['ma_close' + str(i)].fillna(my_df['close' + str(i)])
 	return my_df
 
-def moving_average_rule(df):
-	bool_arr = []
-	for i in range(1, 11):
-		bool_arr.append(np.where(df['close' +str(i)] > df['ma_close'+str(i)], True, False))
-	return 	bool_arr
 
 def max_price(days, df):
 	d = df.copy()
 	for i in range(1,11):
 		d['max_close'+ str(i)] = d['close'+str(i)].shift(1).rolling(window=days).max()
-	d.dropna(inplace = True)
+		d['max_close' + str(i)] = my_df['max_close' + str(i)].fillna(my_df['close' + str(i)])
 	return d
 
-def max_price_rule(df):
-	bool_arr = []
-	for i in range(1, 11):
-		bool_arr.append(np.where(df['close' +str(i)] > df['max_close'+str(i)], True, False))
-	return bool_arr  
 
 def min_price(days, df):
 	d = df.copy()
 	for i in range(1,11):
-		d['max_close'+ str(i)] = d['close'+str(i)].shift(1).rolling(window=days).min()
-	d.dropna(inplace = True)
+		d['min_close'+ str(i)] = d['close'+str(i)].shift(1).rolling(window=days).min()
+		d['min_close' + str(i)] = my_df['min_close' + str(i)].fillna(my_df['close' + str(i)])
 	return d
-
-def min_price_rule(df):
-	bool_arr = []
-	for i in range(1, 11):
-		bool_arr.append(np.where(df['close' +str(i)] > df['max_close'+str(i)], True, False))
-	return bool_arr 
+ 
 
 def main():
 	df = read_file("Training_data.csv")
-	my_df = moving_average(7, df)
-	bool_arr = moving_average_rule(my_df)
-	print(len(bool_arr[0]))
+	my_df = max_price(7, df)
 
 if __name__ == '__main__':
 		main()	
